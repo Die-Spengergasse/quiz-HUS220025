fda646fc9435bfd3d071f27fccaf49c1258ac290
HUS220025 <hus220025@spengergasse.at>
2025-03-17 16:00:47 +0100

    .

--- src/main/java/at/spengergasse/Quiz.java
+++ src/main/java/at/spengergasse/Quiz.java
@@ -15,6 +15,8 @@ public class Quiz
     private List<Question> questions;
     Scanner sc = new Scanner(System.in);
     Question q;
+    int nextQ = 0;
+    int correctAnswers = 0;
 
     public Quiz()
     {
@@ -26,20 +28,70 @@ public class Quiz
 
     public boolean showQaA()
     {
-        List<Answer> answers = q.getAnwerList();
+        List<Answer> answers = q.getAnswerList();
+
         for (int i = 0; i < answers.size(); i++)
         {
+            q = questions.get(i);
+            System.out.println(q.getText());
             Answer a = answers.get(i);
             System.out.println((i+1) + ") " + a.getText());
+
+            System.out.println("your answer: ");
+            int userAnswer = Integer.parseInt(sc.nextLine());
+            checkAnswer(q, userAnswer);
         }
 
-        System.out.println("your answer: ");
-        int userAnswer = Integer.parseInt(sc.nextLine());
-        checkAnswer(q, userAnswer);
+
+        if(nextQ < answers.size())
+        {
+            finish();
+            return false;
+        }
+
         return true;
     }
 
-    private void checkAnswer(List<Question> q, int userAnswer) {
+    private void finish()
+    {
+        double percentage = (double) correctAnswers / questions.size() * 100;
+        System.out.println("You completed the Quiz!");
+        System.out.println("You answered " + correctAnswers + " out of " + questions.size() + " questions correctly.");
+        System.out.println("That's " + percentage + "% correct answers!");
+    }
+
+    private void checkAnswer(Question q, int userAnswer)
+    {
+        List<Answer> answers = q.getAnswerList();
+        if (userAnswer >= 1 && userAnswer <= answers.size())
+        {
+            Answer selectedAnswer = answers.get(userAnswer - 1);
+            if (selectedAnswer.isCorrect())
+            {
+                System.out.println("Correct!");
+                correctAnswers++;
+            }
+            else
+            {
+                System.out.println("Wrong! The correct answer was: " + getCorrectAnswerText(answers));
+            }
+        }
+        else
+        {
+            System.out.println("Invalid answer!");
+        }
+    }
+
+    private String getCorrectAnswerText(List<Answer> answers)
+    {
+        for (Answer answer : answers)
+        {
+            if (answer.isCorrect())
+            {
+                return answer.getText();
+            }
+        }
+        return "No correct answer found!";
     }
 
 }
---
+++ src/main/java/at/spengergasse/Start.java
@@ -0,0 +1,17 @@
+package at.spengergasse;
+
+public class Start
+{
+    public static void main(String[] args) {
+
+        Quiz quiz = new Quiz();
+        boolean show = true;
+
+        while (show)
+        {
+            System.out.println(quiz.showQaA());
+        }
+    }
+
+
+}
